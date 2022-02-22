5e6126ee39f424e1de5f89f5c85b60ca2dba9539
Amit Berger <bergeramit@gmail.com>
2022-02-22 16:45:07 +0200

    Added specific commit to go to since this repo changers repidly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+rm -rf bcc
 git clone https://github.com/iovisor/bcc.git
-mkdir bcc/build; cd bcc/build
+cd bcc && git checkout 552551946a27a8d27086d289a5925b7e75a53ed8
+mkdir build; cd build
 cmake ..
 make
 make install
